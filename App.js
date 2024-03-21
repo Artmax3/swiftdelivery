bc7f5b4d089a920d50d2de85d6fdfd310b5545a8
Mahima Neupane <mahima123@test.com>
2024-03-21 17:32:29 -0400

    Final commit

--- App.js
+++ App.js
@@ -6,8 +6,10 @@ import OrderScreen from './OrderScreen.js';
 import AuthorizationScreen from './AuthorizationScreen.js';
 import AddressScreen from './AddressScreen.js';
 import CheckoutScreen from './CheckoutScreen.js';
-// import OrderUpdatesScreen from './OrderUpdatesScreen';
 import { StripeProvider } from '@stripe/stripe-react-native';
+import AvailableStores from './AvailableStores.js';
+import StoreDetails from './StoreDetails.js';
+import Cart from './Cart.js';
 
 const Stack = createStackNavigator();
 
@@ -20,7 +22,9 @@ const App = () => {
         <Stack.Screen name="Auth" component={AuthorizationScreen} />
         <Stack.Screen name="Address" component={AddressScreen} />
         <Stack.Screen name="Checkout" component={CheckoutScreen} />
-        {/* <Stack.Screen name="OrderUpdates" component={OrderUpdatesScreen} /> */}
+        <Stack.Screen name="Restaurant" component={AvailableStores} />
+        <Stack.Screen name="Menu" component={StoreDetails} />
+        <Stack.Screen name="Cart" component={Cart} />
       </Stack.Navigator>
     </NavigationContainer>
   );
